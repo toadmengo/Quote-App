62600636169652e1d3f223ac760af0dbc866df4f
Todd meng <tftmeng@gmail.com>
2020-12-19 14:44:31 -0800

    Initial Commit

---
+++ Quotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Quotes
+//
+//  Created by Todd Meng on 12/19/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Quotes/QuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuotesApp.swift
+//  Quotes
+//
+//  Created by Todd Meng on 12/19/20.
+//
+
+import SwiftUI
+
+@main
+struct QuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
